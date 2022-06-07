a623270daf3bdbf8d8766dbe5e17c6fe59d30b93
Naganishe <mr.nordakov@mail.ru>
2022-06-07 20:32:49 +0300

    MrthodArray

---
+++ MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1,2,34,54,23,123,54,23};
+
+int n = array.Length;
+int find = 23;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
